6d0797c385d943ab22775f8878f7349dc5bdad8f
Gerson Espinal <gerson.espinal@gmail.com>
2019-01-28 15:55:30 -0500

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -9,12 +9,7 @@
 // }
 
 // const config = {
-//   apiKey: 'AIzaSyDluonuaPcLFWSjnA7h8EaRCKxZnUHJ19g',
-//   authDomain: 'foodber-65c10.firebaseapp.com',
-//   databaseURL: 'https://foodber-65c10.firebaseio.com',
-//   projectId: 'foodber-65c10',
-//   storageBucket: 'foodber-65c10.appspot.com',
-//   messagingSenderId: '669394895252',
+
 // };
 
 // firebase.initializeApp(config);
