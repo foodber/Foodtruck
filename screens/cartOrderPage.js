2424be379a621ae24fe0be4f3c4e494dcb8212d3
Gerson Espinal <gerson.espinal@gmail.com>
2019-02-05 17:12:54 -0500

    Feature: able to receive notifications using expo notification tools

--- screens/cartOrderPage.js
+++ screens/cartOrderPage.js
@@ -1,41 +1,73 @@
-import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { fetchAllOrders } from '../store/Reducer';
-import { allOrders } from '../db/fire'
+import React from "react";
+import { ScrollView, StyleSheet, View, Text, Button } from "react-native";
+import { connect } from "react-redux";
+import { fetchAllOrders } from "../store/Reducer";
+import { firebase, allOrders, expoToken } from "../db/fire";
+import "firebase/auth";
+// import { askForPermissioToReceiveNotifications } from "../db/push-notification";
 
 class LinkScreen extends React.Component {
   constructor() {
     super();
     this.state = {
-      orders: [],
+      orders: []
     };
+    this.register = this.register.bind(this);
   }
 
   static navigationOptions = {
-    title: 'Online Orders',
+    title: "Online Orders"
   };
+
+  async register() {
+    const { status } = await Expo.Permissions.askAsync(
+      Expo.Permissions.NOTIFICATIONS
+    );
+
+    if (status !== "granted") {
+      alert("You need enable permissions in settings");
+      return;
+    }
+
+    const token = await Expo.Notifications.getExpoPushTokenAsync();
+
+    const user = firebase.auth().currentUser;
+    expoToken.doc(user.uid).set({ token });
+    console.log(token);
+  }
+
+  listen({ origin, data }) {
+    console.log(origin, data);
+  }
+
+  componentWillMount() {
+    this.listener = Expo.Notifications.addListener(this.listen);
+  }
+
+  componentWillUnmount() {
+    this.listener && Expo.Notifications.removeListener(this.listen);
+  }
 
   async componentDidMount() {
     // await this.props.fetchAllOrders();
-    const orders = await allOrders.doc('First trucks').get();
-    const orderData = orders.data()
-    let newOrdersData = Object.entries(orderData)
+    const orders = await allOrders.doc("First trucks").get();
+    const orderData = orders.data();
+    let newOrdersData = Object.entries(orderData);
     this.setState({
       orders: newOrdersData
-    })
-    await allOrders.doc('First trucks').onSnapshot(doc => {
-      const newOrders = doc.data()
-      const testing = Object.entries(newOrders)
+    });
+    await allOrders.doc("First trucks").onSnapshot(doc => {
+      const newOrders = doc.data();
+      const testing = Object.entries(newOrders);
       this.setState({
         orders: testing
-      })
-    })
+      });
+    });
   }
 
   render() {
-    // const allOrders = this.props.allOrders || [];
-    const truckOrders = this.state.orders || []
+    console.log(this.state);
+    const truckOrders = this.state.orders || [];
     return (
       <ScrollView>
         <Text style={styles.theHeader}>Incoming Orders: </Text>
@@ -43,20 +75,23 @@ class LinkScreen extends React.Component {
           {truckOrders &&
             truckOrders[0] &&
             truckOrders.map((order, index) => {
-              let eachOrder = Object.entries(order[1])
+              let eachOrder = Object.entries(order[1]);
               return (
-                <View key={index}>
-                  <Text>Ordered Person's Name : {order[0]}</Text>
+                <View key={index} style={styles.individualOrder}>
+                  <Text style={styles.userName}>
+                    Ordered Person's Name : {order[0]}
+                  </Text>
                   {eachOrder.map((singleOrder, index) => {
                     return (
                       <View key={index}>
                         <Text>Ordered Item : {singleOrder[0]}</Text>
                         <Text>Ordered Quantity : {singleOrder[1]}</Text>
                       </View>
-                    )
+                    );
                   })}
+                  <Button title="DONE" onPress={this.register} />
                 </View>
-              )
+              );
             })}
         </View>
       </ScrollView>
@@ -68,39 +103,56 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   padding: {
     paddingTop: 5,
     paddingBottom: 5,
     paddingLeft: 5,
-    paddingRight: 7,
+    paddingRight: 7
   },
   isItWorking: {
-    fontSize: 24,
+    fontSize: 24
   },
   theHeader: {
+    fontWeight: "bold",
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'left',
+    textAlign: "center"
   },
   ViewBox: {
     paddingLeft: 10,
     // borderRadius: 5,
     // borderWidth: 1,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5"
   },
+  individualOrder: {
+    flex: 1,
+    backgroundColor: "#dfe6e9",
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 7
+  },
+  userName: {
+    fontWeight: "bold",
+    fontSize: 18
+  },
+  button: {
+    flex: 1,
+    flexDirection: "row"
+  }
 });
 
 const mapStateToProps = state => ({
-  allOrders: state.allOrders.allOrders,
+  allOrders: state.allOrders.allOrders
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchAllOrders: () => {
     dispatch(fetchAllOrders());
-  },
+  }
 });
 
 export default connect(
