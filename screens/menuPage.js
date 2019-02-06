28aedcb6e514b75c0280bd017c56fd3bd0f5c56f
kj192 <kxjo47@gmail.com>
2019-02-06 15:04:04 -0500

    removed all logs and unnecessary comments

--- screens/menuPage.js
+++ screens/menuPage.js
@@ -9,109 +9,116 @@ import {
   TextInput,
 } from 'react-native';
 import { connect } from 'react-redux';
-import { fetchTruck} from '../store/Reducer';
-import { allTrucks } from '../db/fire'
+import { fetchTruck } from '../store/Reducer';
+import { allTrucks } from '../db/fire';
 
 class SettingsScreen extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       menu: [],
       itemName: '',
-      itemPrice: ''
-    }
-    this.removeItemFromMenu = this.removeItemFromMenu.bind(this)
-    this.addItemToMenu = this.addItemToMenu.bind(this)
+      itemPrice: '',
+    };
+    this.removeItemFromMenu = this.removeItemFromMenu.bind(this);
+    this.addItemToMenu = this.addItemToMenu.bind(this);
   }
   static navigationOptions = {
     title: 'Edit Menu',
   };
 
   async componentDidMount() {
-    // await this.props.fetchTruck('3H9auvKIacpVaD1nPvOD')
-    let data = await allTrucks.doc('3H9auvKIacpVaD1nPvOD').get()
-    let truckData = data.data()
+    let data = await allTrucks.doc('3H9auvKIacpVaD1nPvOD').get();
+    let truckData = data.data();
     this.setState({
-      menu: truckData.menu
-    })
+      menu: truckData.menu,
+    });
   }
 
-  async removeItemFromMenu (itemName) {
-    let currentMenu = this.state.menu
+  async removeItemFromMenu(itemName) {
+    let currentMenu = this.state.menu;
     let newMenu = currentMenu.filter(item => {
-      return item.name !== itemName
-    })
+      return item.name !== itemName;
+    });
     this.setState({
-      menu: newMenu
-    })
+      menu: newMenu,
+    });
     await allTrucks.doc('3H9auvKIacpVaD1nPvOD').update({
-      menu: newMenu
-    })
+      menu: newMenu,
+    });
   }
 
-  async addItemToMenu (itemName, itemPrice) {
+  async addItemToMenu(itemName, itemPrice) {
     if (this.state.itemName.length === 0) {
-      alert("Please enter a product name")
+      alert('Please enter a product name');
     } else if (this.state.itemPrice.length === 0) {
-      alert("Please enter the products price")
+      alert('Please enter the products price');
     } else {
       await allTrucks.doc('3H9auvKIacpVaD1nPvOD').update({
-        menu: [...this.state.menu, {name: itemName, price: itemPrice}]
-      })
+        menu: [...this.state.menu, { name: itemName, price: itemPrice }],
+      });
       this.setState({
-        menu: [...this.state.menu, {name: itemName, price: itemPrice}],
+        menu: [...this.state.menu, { name: itemName, price: itemPrice }],
         itemName: '',
-        itemPrice: ''
-      })
+        itemPrice: '',
+      });
     }
   }
 
   render() {
-    const truckMenu = this.state.menu || []
+    const truckMenu = this.state.menu || [];
     return (
-     <ScrollView>
-      <TextInput
-        placeholder="Please Enter Item Name"
-        onChangeText={text => {
-          this.setState({
-            itemName: text
-          })
-        }}
-        value={this.state.itemName}
-       />
-       <TextInput
-        placeholder="Please Enter Item Price"
-        keyboardType="numeric"
-        value={this.state.itemPrice}
-        maxLength={7}
-        onChangeText={text => {
-          this.setState({
-            itemPrice: text
-          })
-        }}
-       />
-       <Button title="ADD ITEM TO MENU" onPress={() => this.addItemToMenu(this.state.itemName, this.state.itemPrice)} />
-       <View>
-         {truckMenu && truckMenu[0] &&
-          truckMenu.map(menuItem => {
-            return (
-              <View key={menuItem.name}>
-              <Text>Item Name : {menuItem.name}</Text>
-              <Text>Item Price : {menuItem.price}</Text>
-              <Button title="REMOVE ITEM FROM MENU" onPress={() => this.removeItemFromMenu(menuItem.name)} />
-              </View>
-            )
-          })
-        }
-       </View>
-     </ScrollView>
+      <ScrollView>
+        <TextInput
+          placeholder="Please Enter Item Name"
+          onChangeText={text => {
+            this.setState({
+              itemName: text,
+            });
+          }}
+          value={this.state.itemName}
+        />
+        <TextInput
+          placeholder="Please Enter Item Price"
+          keyboardType="numeric"
+          value={this.state.itemPrice}
+          maxLength={7}
+          onChangeText={text => {
+            this.setState({
+              itemPrice: text,
+            });
+          }}
+        />
+        <Button
+          title="ADD ITEM TO MENU"
+          onPress={() =>
+            this.addItemToMenu(this.state.itemName, this.state.itemPrice)
+          }
+        />
+        <View>
+          {truckMenu &&
+            truckMenu[0] &&
+            truckMenu.map(menuItem => {
+              return (
+                <View key={menuItem.name}>
+                  <Text>Item Name : {menuItem.name}</Text>
+                  <Text>Item Price : {menuItem.price}</Text>
+                  <Button
+                    title="REMOVE ITEM FROM MENU"
+                    onPress={() => this.removeItemFromMenu(menuItem.name)}
+                  />
+                </View>
+              );
+            })}
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  truck: state.truckMenu.truckMenu
-})
+  truck: state.truckMenu.truckMenu,
+});
 
 const mapDispatchToProps = dispatch => ({
   fetchTruck: truckId => {
--- store/Reducer.js
+++ store/Reducer.js
@@ -18,31 +18,27 @@ const getOrders = allOrders => {
 const getTrucks = truckMenu => {
   return {
     type: GET_TRUCK_MENU,
-    truckMenu
-  }
-}
+    truckMenu,
+  };
+};
 
-export const fetchTruck = (truckId) => {
+export const fetchTruck = truckId => {
   return async dispatch => {
     try {
-      const truck = await allTrucks.doc(truckId).get()
-      const truckData = truck.data()
-      dispatch(getTrucks(truckData.menu))
+      const truck = await allTrucks.doc(truckId).get();
+      const truckData = truck.data();
+      dispatch(getTrucks(truckData.menu));
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
-}
+  };
+};
 
 export const fetchAllOrders = () => {
   return async dispatch => {
     try {
-      const orders = await allOrders.doc('First trucks').get(); //allOrders.doc(with specific truck Key)
-      //   orders.forEach(anOrder => {
-      //     orderList.push(anOrder.data());
-      //   });
-      let newObj=Object.values(orders.data())
-      console.log('this one >>>>>>>>', newObj);
+      const orders = await allOrders.doc('First trucks').get();
+      let newObj = Object.values(orders.data());
       dispatch(getOrders(orders.data()));
     } catch (error) {
       console.error(error);
